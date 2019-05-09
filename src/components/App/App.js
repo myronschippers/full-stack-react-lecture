3287915336a7214ec78e8626cd24a9222866d15a
Myron <dubtwee@gmail.com>
2019-05-09 13:52:15 -0500

    getting all songs from DB

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    songs: [],
+  }
+  componentDidMount() {
+    console.log('werd');
+    this.getSongs();
+  }
+
+  getSongs() {
+    axios({
+      method: 'GET',
+      url: '/songs',
+    })
+    .then((response) => {
+      console.log(response);
+      this.setState({
+        songs: response.data,
+      })
+    });
+  }
+
   render() {
+    const htmlSongs = this.state.songs.map((indvSong, i) => {
+      return <p key={i}>{indvSong.track} by {indvSong.artist}</p>;
+    });
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Songs!</h1>
         </header>
         <br/>
-        <p>Songs go here</p>
+        {htmlSongs}
       </div>
     );
   }
